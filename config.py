e63df944caa183fb83ad5f44cad63e9bbc03c152
woohaen88 <woohaen88@gmail.com>
2021-11-11 10:20:18 +0000

    폼에 부트스트랩 적용하기

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@testdb/ururu?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = 'dev'
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, render_template
+from datetime import datetime
+from flask import Blueprint, render_template, request, url_for
 from pybo.models import Question
+from werkzeug.utils import redirect
+from .. import db
+from ..models import Question
+from ..forms import QuestionForm
 
 NAME = 'question'
 bp = Blueprint(NAME, __name__, url_prefix='/question')
@@ -15,3 +20,13 @@ def detail(question_id):
     question = Question.query.get_or_404(question_id)
     return render_template('question/question_detail.html',
                             question=question)
+
+@bp.route('/create/', methods=('GET', 'POST'))
+def create():
+    form = QuestionForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index')) # POST 방식 요청이면 데이터 저장 후 질문 목록 페이지로 이동
+    return render_template('question/question_form.html', form=form) # GET 방식 요청이면 질문 등록 페이지 렌더링
